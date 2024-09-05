6ba79825630c73d97cbd947e5c04daa1cd5d169d
The Goose <ryanshaffer1324@gmail.com>
2024-09-05 06:30:19 -0400

    Added contents to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// import npm packs
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+// set port to env port or to 3001
+const PORT = process.env.PORT || 3001;
+// create instance of app set to express
+const app = express();
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// express uses routes
+app.use(routes);
+
+// start the server after the database connection is established
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Connected on localhost:${PORT}`);
+  });
+});
